1a6c0979301d39e9321c8e1ac975c37c56fbf066
Nas Issa <nasissa97@gmail.com>
2025-01-12 15:55:10 -0800

    Basic setup for web layer

---
+++ src/fake/explorer.py
@@ -0,0 +1,33 @@
+from model.explorer import Explorer
+
+_explorers = [
+  Explorer(name="John Doe",
+           country="FN",
+           description="Scarce during full moons"),
+  Explorer(name="Naserati",
+           country="US",
+           description="Faster than a Maserati")
+]
+
+def get_all() -> list[Explorer]:
+  return _explorers
+
+def get_one(name: str) -> Explorer | None:
+  for _explorer in _explorers:
+    if _explorer.name == name:
+      return _explorer
+    
+  return None
+
+def create(explorer: Explorer) -> Explorer:
+  return explorer
+
+def modify(explorer: Explorer) -> Explorer:
+  return explorer
+
+def replace(explorer: Explorer) -> Explorer:
+  return explorer
+
+def delete(name: str) -> bool:
+  return None
+
---
+++ src/web/explorer.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter
+from model.explorer import Explorer
+import fake.explorer as service
+
+router = APIRouter(prefix= "/explorer")
+
+@router.get("/")
+def get_all():
+  return service.get_all()
+
+@router.get("/{name}")
+def get_one(name: str) -> Explorer | None:
+  return service.get_one(name)
+
+@router.post("/")
+def create(explorer: Explorer) -> Explorer:
+  return service.create(explorer)
+
+@router.patch("/")
+def modify(explorer: Explorer) -> Explorer:
+  return service.modify(explorer)
+
+@router.put("/")
+def create(explorer: Explorer) -> Explorer:
+  return service.replace(explorer)
+
+@router.delete("/{name}")
+def create(name: str) -> None:
+  service.delete(name)
+  return None
